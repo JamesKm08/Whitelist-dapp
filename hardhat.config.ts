b8f51e53d53811dc98ae3375fbff6dfb9a879766
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-09-15 11:22:01 +0300

    feat: add environment varibales

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: ".env" });
+
+const INFURA_HTTP_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "^0.8.19",
+  networks: {
+    sepolia: {
+      url: INFURA_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
